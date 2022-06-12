7a498ed41f68a5d58102c897646fae501ad2697d
Carlos Magalhães Silva <carlosmsilva755@gmail.com>
2022-06-12 16:20:51 -0300

    Update main.c

--- Montador/main.c
+++ Montador/main.c
@@ -5,7 +5,53 @@
 void check_intruction(char *line){
     if(strstr(line,"LOAD") != NULL){
         printf("01");
-    }else{
+    }
+    if(strstr(line,"STORE") != NULL){
+        printf("02");
+    }
+    if(strstr(line,"ADD") != NULL){
+        printf("03");
+    }
+    if(strstr(line,"SUB") != NULL){
+        printf("04");
+    }
+    if(strstr(line,"JMP") != NULL){
+        printf("05");
+    }
+    if(strstr(line,"JPG") != NULL){
+        printf("06");
+    }
+    if(strstr(line,"JPL") != NULL){
+        printf("07");
+    }
+    if(strstr(line,"JPE") != NULL){
+        printf("08");
+    }
+    if(strstr(line,"JPNE") != NULL){
+        printf("09");
+    }
+    if(strstr(line,"PUSH") != NULL){
+        printf("10");
+    }
+    if(strstr(line,"POP") != NULL){
+        printf("11");
+    }
+    if(strstr(line,"READ") != NULL){
+        printf("12");
+    }
+    if(strstr(line,"WRITE") != NULL){
+        printf("13");
+    }
+    if(strstr(line,"CALL") != NULL){
+        printf("14");
+    }
+    if(strstr(line,"RET") != NULL){
+        printf("15");
+    }
+    if(strstr(line,"HALT") != NULL){
+        printf("16");
+    }
+    else{
         printf("%s",line);
     }
 }
